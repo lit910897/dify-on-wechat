9b8e6ed816f1e48c34662045713275b8615b57ba
lit910897 <75847713+lit910897@users.noreply.github.com>
2026-07-03 10:00:10 +0800

    Update channel_factory.py

--- channel/channel_factory.py
+++ channel/channel_factory.py
@@ -42,6 +42,9 @@ def create_channel(channel_type) -> Channel:
     elif channel_type == const.DINGTALK:
         from channel.dingtalk.dingtalk_channel import DingTalkChanel
         ch = DingTalkChanel()
+    elif channel_type == "line":
+        from channel.line.line_channel import LineChannel
+        ch = LineChannel()
     else:
         raise RuntimeError
     ch.channel_type = channel_type
